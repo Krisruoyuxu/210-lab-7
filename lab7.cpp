7b49185a3520881f3eafe4cea7aebe5eea7f3948
dickxumamba <xuruoyu27@gmail.com>
2025-09-15 07:57:00 +0800

    second commit, finish the structure and the displayarray function

--- lab7.cpp
+++ lab7.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+const int SIZE = 5;
+
+void reverseArray(string*arr);
+void displayArray(string*arr);
 
 int main() {
     
-    //
+    string* array = new string[SIZE]{"Kris","Ruoyu","Xu","Yun","Zhu"};
+
+    displayArray(array);
 
     return 0;
+}
+
+void reverseArray(string*arr){
+
+}
+
+void displayArray(string*arr){
+    cout << "Reversed array: ";
+    for (int i = 0; i < SIZE; i++){
+        cout << *(arr+i) <<" ";
+    }
+    cout <<endl;
 }
