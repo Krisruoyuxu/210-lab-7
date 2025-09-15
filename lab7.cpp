954ccfe4f7c9dacff07b0e5c53079f5b4c654faf
dickxumamba <xuruoyu27@gmail.com>
2025-09-15 08:20:19 +0800

    Finish the first draft, but ouput not as expected, i'm working on it

--- lab7.cpp
+++ lab7.cpp
@@ -5,28 +5,30 @@ using namespace std;
 const int SIZE = 5;
 
 void reverseArray(string*arr);
-void displayArray(string*arr);
+void displayArray(string*arr, string type);
 
 int main() {
     
     string* array = new string[SIZE]{"Kris","Ruoyu","Xu","Yun","Zhu"};
 
-    displayArray(array);
+    displayArray(array, "Original array");
+    reverseArray(array);
+    displayArray(array, "Reversed array");
 
     return 0;
 }
 
 void reverseArray(string*arr){
     string temp;
-    for (int i = 0; i < SIZE; i++){
+    for (int i = 0; i < (SIZE / 2); i++){
         temp = *(arr+i);
         *(arr+i) = *(arr+SIZE-1);
-        
+        *(arr+SIZE-1) = temp;
     }
 }
 
-void displayArray(string*arr){
-    cout << "Reversed array: ";
+void displayArray(string*arr, string type){
+    cout << type <<": ";
     for (int i = 0; i < SIZE; i++){
         cout << *(arr+i) <<" ";
     }
